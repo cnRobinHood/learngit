8745692f1c76a6e9edbe7701b2266de0a25d8fab
cnRobinHood <iamliucheng@gmail.com>
2016-11-19 00:10:30 +0800

    Robin

--- RedRock0/FindWordsDemo.java
+++ RedRock0/FindWordsDemo.java
@@ -8,11 +8,14 @@ import java.util.Scanner;
 public class FindWordsDemo {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("请输入你要查找关键字的文件的绝对路径和关键字：");
+        System.out.println("请输入你要查找关键字的文件的绝对路径：");
         String Ap = scanner.nextLine();
         FindWords findWords = new FindWords(Ap);
         System.out.println("请输入你要查找的关键字：");
         String keyword = scanner.nextLine();
-        findWords.Find(keyword);
+        for (Object a : findWords.Find(keyword)
+                ) {
+            System.out.println(a);
+        }
     }
 }
